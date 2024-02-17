c3f5ef1ea44e7bbb791b721343b6fea1a5e6c790
mohitparmar1 <contact.mohit06@gmail.com>
2024-02-17 18:12:45 +0530

    frontend: Created Custom Hook for handling Create user using React-query

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,11 +1,16 @@
+import { useMyUserRequest } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 import React from "react";
+import { useMutation } from "react-query";
 
 type Props = {
     children: React.ReactNode;
 }
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+
+    const { createUser } = useMyUserRequest();
+
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL
@@ -15,7 +20,12 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     }
 
     const onredirectCallback = (appState?: AppState, user?: User) => {
-        console.log(appState, user);
+      if(user?.sub && user?.email){
+        createUser({
+            auth0Id : user.sub,
+            email: user.email
+        })
+      }
     }
 
     return (
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,26 @@ import './global.css'
 import { BrowserRouter as Router } from "react-router-dom"
 import AppRoutes from './AppRoutes'
 import AuthProviderWithNavigate from './auth/Auth0ProviderWithNavigate'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
-      <AuthProviderWithNavigate>
-        <AppRoutes />
-      </AuthProviderWithNavigate>
+      <QueryClientProvider
+        client={queryClient}
+      >
+        <AuthProviderWithNavigate>
+          <AppRoutes />
+        </AuthProviderWithNavigate>
+      </QueryClientProvider>
     </Router>
   </React.StrictMode>,
 )
