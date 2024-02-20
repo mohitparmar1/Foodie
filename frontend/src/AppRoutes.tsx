da7420198f67748427d1f5687a8d5cf954022846
mohitparmar1 <contact.mohit06@gmail.com>
2024-02-20 23:34:30 +0530

    userProfile form With React-hook-form

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -2,12 +2,15 @@ import { Navigate, Route, Routes } from "react-router-dom"
 import Layout from "./layouts/Layout"
 import HomePage from "./pages/HomePage"
 import AuthCallBack from "./pages/AuthCallBack"
+import UserProfilePage from "./pages/UserProfilePage"
 
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<Layout><HomePage /></Layout>} />
-            <Route path="/user-profile" element={<span>User Profile Page</span>} />
+            <Route path="/" element={<Layout showHero><HomePage /></Layout>} />
+            <Route path="/user-profile" element={<Layout>
+                <UserProfilePage />
+            </Layout>} />
             <Route path="/auth-callback" element={<AuthCallBack />} />
             <Route path="*" element={<Navigate to="/" />} />
         </Routes>
