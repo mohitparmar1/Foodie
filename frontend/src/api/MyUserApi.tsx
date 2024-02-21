b0fc55c832d603bf793cac1c352daa0369876269
mohitparmar1 <contact.mohit06@gmail.com>
2024-02-21 23:38:00 +0530

    frontend: UseGetUser Hook

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -1,8 +1,37 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { useMutation } from "react-query";
+import { useMutation, useQuery } from "react-query";
 import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useGetMyUser = () => {
+    const { getAccessTokenSilently } = useAuth0();
+    const getMyUserRequest = async () => {
+        const accessToken = await getAccessTokenSilently();
+        const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+            method: "GET",
+            headers: {
+                authorization: `Bearer ${accessToken}`,
+                "Content-Type": "application/json"
+            }
+        })
+        if (!response.ok) {
+            throw new Error("Failed to fetch user")
+        }
+        return response.json();
+    };
+    const {
+        data: currentUser,
+        isLoading,
+        error
+    } = useQuery("fetchMyUser", getMyUserRequest);
+
+    return {
+        currentUser,
+        isLoading,
+        error,
+    }
+}
 
 type CreateUserRequest = {
     auth0Id: string;
