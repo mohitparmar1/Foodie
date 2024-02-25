3c233e5fdb5cce8624a83db10d45acf0ec0db30c
mohitparmar1 <contact.mohit06@gmail.com>
2024-02-26 00:04:52 +0530

    setup multer

--- backend/src/controllers/MyRestaurantController.ts
+++ backend/src/controllers/MyRestaurantController.ts
@@ -3,7 +3,22 @@ import Restaurant from "../models/restaurant"
 
 
 const createMyRestaurant = async (req: Request, res: Response) => {
-   
+    try {
+        const existingRestaurant = await Restaurant.find({
+            user: req.userId
+        })
+        if (existingRestaurant) {
+            return res.status(409).json({
+                message: "User Restaurant already exists"
+            })
+        }
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message: "Error creating restaurant"
+        })
+    }
 
 };
 
